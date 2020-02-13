85b837287eff6afff903cab4b7d15cec5f79faee
Mohammed Adinan <adinanaries@outlook.com>
2020-02-13 12:23:54 -0500

    calculated dates for day2 through to day 5 for forcust displays

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var Week = [
   "Sunday",
   "Monday",
   "Tuesday",
-  "Wednessday",
+  "Wednesday",
   "Thursday",
   "Friday",
   "Saturday"
 ];
+
+var ShortWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 var Month = [
   "January",
   "February",
@@ -28,15 +31,112 @@ var Today = new Date();
 const Day2 = new Date(Today);
 Day2.setDate(Day2.getDate() + 1);
 
+//setting day 2 day and date
+
+var Day2Date = Day2.getDate();
+var Day2DisplayDate;
+
+var Day2Suffix = "th";
+
+if (Day2Date.toString().charAt(Day2Date.toString.length - 1) === "1")
+  Day2Suffix = "st";
+
+if (Day2Date.toString().charAt(Day2Date.toString.length - 1) === "2")
+  Day2Suffix = "nd";
+
+if (Day2Date.toString().charAt(Day2Date.toString.length - 1) === "3")
+  Day2Suffix = "rd";
+if (Day2Date.toString().length === 2) {
+  if (Day2Date.toString().charAt(Day2Date.toString.length - 1) === "1")
+    //alert(DayOfMonth.toString().charAt(DayOfMonth.toString.length - 2));
+    Day2Suffix = "th";
+}
+
+Day2DisplayDate = Day2Date + Day2Suffix + ", " + ShortWeek[Day2.getDay()];
+document.getElementById("Day2").innerText = Day2DisplayDate;
+
 const Day3 = new Date(Day2);
 Day3.setDate(Day3.getDate() + 1);
+
+//setting day 3 day and date
+
+var Day3Date = Day3.getDate();
+var Day3DisplayDate;
+
+var Day3Suffix = "th";
+
+if (Day3Date.toString().charAt(Day3Date.toString.length - 1) === "1")
+  Day3Suffix = "st";
+
+if (Day3Date.toString().charAt(Day3Date.toString.length - 1) === "2")
+  Day3Suffix = "nd";
+
+if (Day3Date.toString().charAt(Day3Date.toString.length - 1) === "3")
+  Day3Suffix = "rd";
+if (Day3Date.toString().length === 2) {
+  if (Day3Date.toString().charAt(Day3Date.toString.length - 1) === "1")
+    //alert(DayOfMonth.toString().charAt(DayOfMonth.toString.length - 2));
+    Day3Suffix = "th";
+}
+
+Day3DisplayDate = Day3Date + Day3Suffix + ", " + ShortWeek[Day3.getDay()];
+document.getElementById("Day3").innerText = Day3DisplayDate;
 
 const Day4 = new Date(Day3);
 Day4.setDate(Day4.getDate() + 1);
 
+//setting day 2 day and date
+
+var Day4Date = Day4.getDate();
+var Day4DisplayDate;
+
+var Day4Suffix = "th";
+
+if (Day4Date.toString().charAt(Day4Date.toString.length - 1) === "1")
+  Day4Suffix = "st";
+
+if (Day4Date.toString().charAt(Day4Date.toString.length - 1) === "2")
+  Day4Suffix = "nd";
+
+if (Day4Date.toString().charAt(Day4Date.toString.length - 1) === "3")
+  Day4Suffix = "rd";
+if (Day4Date.toString().length === 2) {
+  if (Day4Date.toString().charAt(Day4Date.toString.length - 1) === "1")
+    //alert(DayOfMonth.toString().charAt(DayOfMonth.toString.length - 2));
+    Day4Suffix = "th";
+}
+
+Day4DisplayDate = Day4Date + Day4Suffix + ", " + ShortWeek[Day4.getDay()];
+document.getElementById("Day4").innerText = Day4DisplayDate;
+
 const Day5 = new Date(Day4);
 Day5.setDate(Day5.getDate() + 1);
 
+//setting day 2 day and date
+
+var Day5Date = Day5.getDate();
+var Day5DisplayDate;
+
+var Day5Suffix = "th";
+
+if (Day5Date.toString().charAt(Day5Date.toString.length - 1) === "1")
+  Day5Suffix = "st";
+
+if (Day5Date.toString().charAt(Day5Date.toString.length - 1) === "2")
+  Day5Suffix = "nd";
+
+if (Day5Date.toString().charAt(Day5Date.toString.length - 1) === "3")
+  Day5Suffix = "rd";
+if (Day5Date.toString().length === 2) {
+  if (Day5Date.toString().charAt(Day5Date.toString.length - 1) === "1")
+    //alert(DayOfMonth.toString().charAt(DayOfMonth.toString.length - 2));
+    Day5Suffix = "th";
+}
+
+Day5DisplayDate = Day5Date + Day5Suffix + ", " + ShortWeek[Day5.getDay()];
+document.getElementById("Day5").innerText = Day5DisplayDate;
+
+//working around current day
 var CurrentMonth = Today.getMonth(); //returns current month
 
 var DayOfWeek = Today.getDay(); //returns Day of Week
